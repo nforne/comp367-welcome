28ab6796f7811145ac41a55395551f02e6859740
Martin Nforne <mnforne@my.centennialcollege.ca>
2026-02-28 23:32:17 -0500

    Add greeting to name and welcome message template display

--- src/main/java/nforne/GreetingController.java
+++ src/main/java/nforne/GreetingController.java
@@ -10,6 +10,8 @@ public class GreetingController {
 
     @GetMapping("/")
     public String greet(@RequestParam(required = false) Integer hourOverride) {
+        // "to display "Good morning, <your name (e.g., John)>, Welcome to COMP367"; 
+        // if it is the afternoon then it should display "Good afternoon, <your name (e.g., John)>, Welcome to COMP367 ""
         int hour = (hourOverride != null) ? hourOverride : LocalTime.now().getHour();
         String part = (hour < 12) ? "Good morning" : "Good afternoon";
         return String.format("%s, Martin, Welcome to COMP367", part);
